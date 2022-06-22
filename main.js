b7ad7142f98d16ff3340631ae90a3b94ca28697b
Vito Farenga <vitofarenga@libero.it>
2022-06-22 16:04:42 +0200

    codice funzionante ma è come se ripetesse le azioni per 5 volte

--- main.js
+++ main.js
@@ -2,6 +2,16 @@ const userWord = prompt("Insrisci una parola");
 
 function palindromo(stringa){
     i = stringa.length
-    for(i = stringa.length; i >=0; i--);
-    console.log
+    let nuovaStringa = "";
+    for(i = stringa.length -1; i >=0; i--){
+        nuovaStringa += stringa[i];
+        if(nuovaStringa == stringa){
+            console.log("La parola è palindroma");
+        }
+        else{
+            console.log("La parola non è palindroma");
+        }
+    }
+    console.log(nuovaStringa);
 };
+palindromo(userWord);
