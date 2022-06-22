b8bcbe7f94742e1dfa94563a45df5e496c06cc23
Vito Farenga <vitofarenga@libero.it>
2022-06-22 16:22:44 +0200

    funzionante

--- main.js
+++ main.js
@@ -5,13 +5,12 @@ function palindromo(stringa){
     let nuovaStringa = "";
     for(i = stringa.length -1; i >=0; i--){
         nuovaStringa += stringa[i];
-        if(nuovaStringa == stringa){
-            console.log("La parola è palindroma");
-        }
-        else{
-            console.log("La parola non è palindroma");
-        }
     }
-    
+    if(nuovaStringa == stringa){
+        console.log("La parola è palindroma");
+    }
+    else{
+        console.log("La parola non è palindroma");
+    }  
 };
 palindromo(userWord);
