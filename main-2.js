a5708c6a895f99237756b1e081bc785525cd10ac
Vito Farenga <vitofarenga@libero.it>
2022-06-22 16:47:20 +0200

    pari e dispari

---
+++ main-2.js
@@ -0,0 +1,33 @@
+const userChoice = parseInt(prompt ("Scegliere un numero da 1 a 5"));
+const userChoiceDue = prompt ("Scegliere pari o dispari");
+
+if((userChoice <= 5) && (userChoice != 0)){
+    console.log("Il tuo numero è: " + userChoice);
+}
+else{
+    alert("Il numero inserito non è valido");
+}
+
+
+function randomIa(){
+    iaChoice = Math.floor( (Math.random() * 5) + 1);
+}
+randomIa(userChoice)
+console.log("Il numero dell'IA è: " + iaChoice );
+
+const sum = userChoice + iaChoice;
+console.log("La somma dei numeri è: " + sum)
+
+function pariDispari(numero){
+    if(numero % 2 == 0){
+        console.log("Il numero è pari");
+    }
+    else{
+        console.log("Il numero è dispari");
+    }
+}
+
+pariDispari(sum);
+
+
+
