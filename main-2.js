7b5c67dd4b2f888e65721a5b130664850c284bd4
Vito Farenga <vitofarenga@libero.it>
2022-06-22 16:51:41 +0200

    anche pari e dispari finito e funzionante

--- main-2.js
+++ main-2.js
@@ -16,7 +16,7 @@ randomIa(userChoice)
 console.log("Il numero dell'IA è: " + iaChoice );
 
 const sum = userChoice + iaChoice;
-console.log("La somma dei numeri è: " + sum)
+console.log("La somma dei numeri è: " + sum);
 
 function pariDispari(numero){
     if(numero % 2 == 0){
@@ -28,6 +28,16 @@ function pariDispari(numero){
 }
 
 pariDispari(sum);
+
+function vincitore(numeroVincente){
+    if(numeroVincente == userChoiceDue){
+        console.log("Complimenti hai vinto")
+    }
+    else{
+        console.log("Hai perso, ritenta")
+    }
+}
+vincitore(sum)
 
 
 
